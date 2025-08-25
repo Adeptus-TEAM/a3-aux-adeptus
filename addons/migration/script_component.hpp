f73a41b48b2f27b4a575c065ce446ab969130070
Bragg <maxime.brochet.98@gmail.com>
2025-08-25 18:42:59 +0200

    Suppression des fichiers obsolètes liés à la migration

--- addons/migration/script_component.hpp
+++
@@ -1,9 +0,0 @@
-#define COMPONENT Migration
-#define COMPONENT_BEAUTIFIED Migration
-#include "\x\ar\addons\main\script_mod.hpp"
-
-// #define DEBUG_MODE_FULL
-// #define ENABLE_PERFORMANCE_COUNTERS
-
-#include "\x\ar\addons\main\script_macros.hpp"
-#include "script_macros.hpp"
