de8c417c02bc9873218b0e821921fc4769dfbb34
RadionWave <66466162+RadionWave@users.noreply.github.com>
2026-02-11 20:26:48 +0100

    Ajout des différentes dépendances

--- addons/weapons_grenades/config.cpp
+++ addons/weapons_grenades/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches {
         name = COMPONENT_NAME;
         addonRootClass = QUOTE(ADDON);
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {QUOTE(ADDON), "3AS_Deka", "3AS_Equipment","JLTS_weapons_Grenades"};
+        requiredAddons[] = {QUOTE(ADDON), "3AS_Deka", "3AS_Equipment","JLTS_weapons_Grenades","ls_characters_droid","WBK_Droids_3AS","WBK_Droids_LS","JLTS_characters_DroidArmor","3AS_Characters_Droids_TSeries"};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
