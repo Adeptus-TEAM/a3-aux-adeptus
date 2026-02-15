53b8cc55890fc4c38aaad48eabe14d6c2012495d
Bragg-0 <189566294+Bragg-0@users.noreply.github.com>
2026-02-15 14:58:43 +0100

    Ajout d'une nouvelle icône pour la grenade IEM et mise à jour du chemin d'accès à l'image

--- addons/weapons_grenades/CfgMagazines.hpp
+++ addons/weapons_grenades/CfgMagazines.hpp
@@ -8,6 +8,6 @@ class CfgMagazines {
         displayName = CSTRING(emp_displayName);
         displayNameShort = CSTRING(emp_displayName);
         model = "\3AS\3AS_Equipment\model\3AS_thermaldet.p3d";
-        picture = "\3AS\3AS_Equipment\UI\Thermal_Detonator_UI_ca.paa";
+        picture = QPATHTOF(data\EMP_Detonator_ca.paa);
     };
 };
